f1dcef7b9bd122c24482a920888f54e18f974d69
Daniel Gajeski <dtgajeski@gmail.com>
2024-04-16 08:49:21 -0700

    Light Structure Planning Update.  Main and UI swapping / loading file in planning stages.

---
+++ ui_manager.py
@@ -0,0 +1,3 @@
+#file meant to be where UIS get INITIALIZED and SWAPPED out for one another.
+
+#Possible new origin point after main.   Load all necessary UI elements into this file and then enable proper UI swapping to take place here.  
